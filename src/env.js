6c3ebd46c7ff4765e0896b68094dbc756f64f500
Angelos Karagkiozidis <angelkar@gmail.com>
2015-11-18 16:37:23 +0200

    Update URL retrieval

--- src/env.js
+++ src/env.js
@@ -4,8 +4,8 @@ export function getStagingEnvironments() {
   let environments = [];
   for (let item in ENV_VARIABLES) {
     if (/OBSERVE_SERVER_/.test(item)) {
-      let [name, url] = ENV_VARIABLES[item].split(',');
-      environments.push({ name, url });
+      let url = ENV_VARIABLES[item];
+      environments.push({ url });
     }
   }
   return environments;
